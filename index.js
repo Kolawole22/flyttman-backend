e9e843625f63905ee799da1d45ab038f3ed5773b
Iwalewa Kolawole <kolawoleiwalewa@gmail.com>
2025-01-26 20:00:34 +0100

    minor change

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use(
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-      sameSite: "none", //process.env.NODE_ENV === "production" ? "none" : "lax",
+      // sameSite: "none", //process.env.NODE_ENV === "production" ? "none" : "lax",
     },
     name: "sessionId",
   })
